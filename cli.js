db80f93c39e1be5e9c1a418f8c8c2d67c141cf93
Matthias Leutenegger <2-mleutenegger@users.noreply.git.syntro.ch>
2020-09-16 10:27:26 +0200

    ADD: commands

--- cli.js
+++ cli.js
@@ -1,18 +1,38 @@
 #!/usr/bin/env node
+
 require('yargs') // eslint-disable-line
-  .command('serve [port]', 'start the server', (yargs) => {
-    yargs
-      .positional('port', {
-        describe: 'port to bind on',
-        default: 5000
-      })
-  }, (argv) => {
-    if (argv.verbose) console.info(`start server on :${argv.port}`)
-    serve(argv.port)
-  })
-  .option('verbose', {
-    alias: 'v',
-    type: 'boolean',
-    description: 'Run with verbose logging'
-  })
-  .argv
+    // .command('serve [port]', 'start the server', (yargs) => {
+    //   yargs
+    //     .positional('port', {
+    //       describe: 'port to bind on',
+    //       default: 5000
+    //     })
+    // }, (argv) => {
+    //   if (argv.verbose) console.info(`start server on :${argv.port}`)
+    //   serve(argv.port)
+    // })
+
+    // Serve the devenv
+    .command('serve', 'start the devenv', (yargs) => {
+
+    }, (argv) => {
+        if (argv.verbose) console.info(`starting devenv`)
+    })
+    // stop the devenv
+    .command('stop', 'stop the devenv', (yargs) => {
+
+    }, (argv) => {
+        if (argv.verbose) console.info(`stopping the devenv`)
+    })
+    // init a new env (provides default compose files)
+    .command('init', 'init a devenv', (yargs) => {
+
+    }, (argv) => {
+        if (argv.verbose) console.info(`initializing devenv`)
+    })
+    .option('verbose', {
+        alias: 'v',
+        type: 'boolean',
+        description: 'Run with verbose logging'
+    })
+    .argv
